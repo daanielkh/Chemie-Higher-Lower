b5e9e74d51149520df7ad5c4e6046e08eae395aa
Hallo0611 <98829015+Hallo0611@users.noreply.github.com>
2022-02-01 16:26:05 +0100

    Update NicoHigherLower.js

--- NicoHigherLower.js
+++ NicoHigherLower.js
@@ -11,7 +11,31 @@ data = [
       name: 'Methanol',
         siede_temperatur: 64.7,
         dichte: 0,79,
-        schmelz_temperatur: -97,6
+        schmelz_temperatur: -97.6
+        
+    },
+    
+    {
+      name: 'Ethanol',
+        siede_temperatur: 78.4,
+        dichte: 0.79,
+        schmelz_temperatur: -114.1
+        
+    },
+    
+    {
+      name: 'Helium',
+        siede_temperatur: -268.9,
+        dichte: 0.18,
+        schmelz_temperatur: -272.2
+        
+    },
+    
+    {
+      name: 'Neon',
+        siede_temperatur: -246.1,
+        dichte: -248,
+        schmelz_temperatur: 0.9
         
     },
     
