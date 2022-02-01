7bdaefc1ee769992888a952eb394d0a1f8905b86
daniiiik <98829479+daniiiik@users.noreply.github.com>
2022-02-01 16:53:31 +0100

    Update LinusHigherLower.js

--- LinusHigherLower.js
+++ LinusHigherLower.js
@@ -15,11 +15,108 @@ data = [
         schmelz_temperatur: 5
     }, 
     
-    {
-        name: 'x',
-        siede_temperatur: x,
+  {
+        name: 'Edelstahl',
+        siede_temperatur: 1500,
         dichte: x,
-        schmelz_temperatur: x
+        schmelz_temperatur: 1500,
     },    
+  {
+        name: 'Quecksilber',
+        siede_temperatur: 1500,
+        dichte: 13.54,
+        schmelztemperatur: -38.4,
+  },
+  
+      {
+        name: 'Wasserstoff',
+        siede_temperatur: -253,
+        dichte: 0.09,
+       schmelz_temperatur: -259,
+    },   
+  
+    {
+        name: 'Natrium',
+        siede_temperatur: 883,
+        dichte: 0.97,
+        schmelz_temperatur: 97.8,
+    },   
+  
+    {
+        name: 'Rubidium',
+        siede_temperatur: 688,
+        dichte: 1.53,
+        schmelz_temperatur: 39.3,
+    },   
+      {
+        name: 'Caesium',
+        siede_temperatur: 678,
+        dichte: 1.9,
+        schmelz_temperatur: 28.4,
+    },   
+     
+    {
+        name: 'Beryllium',
+        siede_temperatur: 2970,
+        dichte: 1.85,
+        schmelz_temperatur: 1287,
+    },   
+  
+    {
+        name: 'Magnesium',
+        siede_temperatur: 1090,
+        dichte: 1.75,
+        schmelz_temperatur: 650,
+    },   
+  
+    {
+        name: 'Calcium',
+        siede_temperatur: 1484,
+        dichte: 1.55,
+        schmelz_temperatur: 842,
+    }, 
+  
+    {
+        name: 'Strontium',
+        siede_temperatur: 1384,
+        dichte: 2.63,
+        schmelz_temperatur: 777,
+    },   
+  
+    {
+        name: 'Barium',
+        siede_temperatur: 1140,
+        dichte: 3.62,
+        schmelz_temperatur: 727,
+    },  
+  
+    {
+        name: 'Bor',
+        siede_temperatur: 2550,
+        dichte: 2.46,
+        schmelz_temperatur: 2076,
+    },   
     
+    {
+        name: 'Gallium',
+        siede_temperatur: 2403,
+        dichte: 5.9,
+        schmelz_temperatur: 29.8,
+    }, 
+  
+    {
+        name: 'Indium',
+        siede_temperatur: 2000,
+        dichte: 7.31,
+        schmelz_temperatur: 256.6,
+    },   
+        
+    {
+        name: 'Thalium',
+        siede_temperatur: 1457,
+        dichte: 11.85,
+        schmelz_temperatur: 30
+    },   
+     
+  
 ]
