354af8d4a1f6f6e5918dae3a2730ea439adc2f5b
daniiiik <98829479+daniiiik@users.noreply.github.com>
2022-02-01 17:04:03 +0100

    Update LinusHigherLower.js

--- LinusHigherLower.js
+++ LinusHigherLower.js
@@ -115,8 +115,85 @@ data = [
         name: 'Thalium',
         siede_temperatur: 1457,
         dichte: 11.85,
-        schmelz_temperatur: 30
+        schmelz_temperatur: 304,
     },   
      
+    {
+        name: 'Kohlenstoff',
+        siede_temperatur: 4827,
+        dichte: x,
+        schmelz_temperatur: 3550,
+    },   
+  
+    {
+        name: 'Silicium',
+        siede_temperatur: 2355,
+        dichte: 2.35,
+        schmelz_temperatur: 1410,
+    },   
+  
+    {
+        name: 'Sauerstoff',
+        siede_temperatur: -183,
+        dichte: 1.43,
+        schmelz_temperatur: -218,
+    },   
+  
+    {
+        name: 'Schwefel',
+        siede_temperatur: 445,
+        dichte: 2.07,
+        schmelz_temperatur: 119,
+    }, 
+  
+    {
+        name: 'Selen',
+        siede_temperatur: 685,
+        dichte: 3.61,
+        schmelz_temperatur: 220,
+    },  
+  
+  
+   {
+        name: 'Tellur',
+        siede_temperatur: 990,
+        dichte: 6.24,
+        schmelz_temperatur: 449,5
+    },   
+  
+    {
+        name: 'Antimon',
+        siede_temperatur: 1750,
+        dichte: 6.7,
+        schmelz_temperatur: 630.6,
+    },  
+  
+    {
+        name: 'Stickstoff',
+        siede_temperatur: -196,
+        dichte: 1.25,
+        schmelz_temperatur: -210,
+    },   
+  
+    {
+        name: 'Fluor',
+        siede_temperatur: -188,
+        dichte: 1.72,
+        schmelz_temperatur: -220,
+    },   
+  
+  {
+        name: 'Chlor',
+        siede_temperatur: -35,
+        dichte: 3.22,
+        schmelz_temperatur: 101.5,
+    },   
+  
+    {
+        name: 'Brom',
+        siede_temperatur: 59,
+        dichte: 3.12,
+        schmelz_temperatur: -7,
+    },   
   
 ]
