06fa59578c5a6c6a333dab0c6c08cfeb17a11f24
Dani K <95299224+DanielKholodov@users.noreply.github.com>
2022-02-01 16:04:41 +0100

    Create LinusHigherLower.js

---
+++ LinusHigherLower.js
@@ -0,0 +1,25 @@
+data = [
+  //Hallo lieber Linus
+    {
+        name: 'Wasser',
+        siede_temperatur: 100,
+        dichte: 5,
+        schmelz_temperatur: 5
+        
+    },
+    
+    {
+        name: 'Kupfer',
+        siede_temperatur: 2562,
+        dichte: 5,
+        schmelz_temperatur: 5
+    }, 
+    
+    {
+        name: 'x',
+        siede_temperatur: x,
+        dichte: x,
+        schmelz_temperatur: x
+    },    
+    
+]
