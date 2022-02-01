35ca9b41090901038befa47cbd688c97e0b1d960
Hallo0611 <98829015+Hallo0611@users.noreply.github.com>
2022-02-01 16:43:35 +0100

    Update NicoHigherLower.js

--- NicoHigherLower.js
+++ NicoHigherLower.js
@@ -40,6 +40,126 @@ data = [
     },
     
     {
+      name: 'Argon',
+        siede_temperatur: -185.5,
+        dichte: 1.78,
+        schmelz_temperatur: -189.4
+        
+    },
+    
+    {
+      name: 'Krypton',
+        siede_temperatur: -153.4,
+        dichte: 3.78,
+        schmelz_temperatur: -157.4
+        
+    },
+    
+    {
+      name: 'Xenon',
+        siede_temperatur: -108.1,
+        dichte: 5.89,
+        schmelz_temperatur: -111.8
+        
+    },
+    
+    {
+      name: 'Phosphor',
+        siede_temperatur: 280.5,
+        dichte: 1.88,
+        schmelz_temperatur: 44.1
+        
+    },
+    
+    {
+      name: 'Arsen',
+        siede_temperatur: 816.8,
+        dichte: 3.74,
+        schmelz_temperatur: 613.1
+        
+    },
+    
+    {
+      name: 'Germanium',
+        siede_temperatur: 2833,
+        dichte: 5.3,
+        schmelz_temperatur: 938.2
+        
+    },
+    
+    {
+      name: 'Iod',
+        siede_temperatur: 184.3,
+        dichte: 4.94,
+        schmelz_temperatur: 113.7
+        
+    },
+    
+    {
+      name: 'Zinn',
+        siede_temperatur: 2602,
+        dichte: 7.3,
+        schmelz_temperatur: 231
+        
+    },
+    
+    {
+      name: 'Lithium',
+        siede_temperatur: 1347,
+        dichte: 0.53,
+        schmelz_temperatur: 180
+        
+    },
+    
+    {
+      name: 'Kalium',
+        siede_temperatur: 774,
+        dichte: 0.86,
+        schmelz_temperatur: 63.5
+        
+    },
+    
+    {
+      name: 'Propan',
+        siede_temperatur: 97,
+        dichte: 0.78,
+        schmelz_temperatur: -126
+        
+    },
+    
+    {
+      name: 'Methan',
+        siede_temperatur: 65,
+        dichte: 0.72,
+        schmelz_temperatur: -182
+        
+    },
+    
+    {
+      name: 'Blei',
+        siede_temperatur: 1740,
+        dichte: 11.34,
+        schmelz_temperatur: 327.4
+        
+    },
+    
+    {
+      name: 'Gold',
+        siede_temperatur: 2700,
+        dichte: 19.3,
+        schmelz_temperatur: 1064
+        
+    },
+    
+    {
+      name: 'Silber',
+        siede_temperatur: 2162,
+        dichte: 10.5,
+        schmelz_temperatur: 660.3
+        
+    },
+    
+    {
         name: 'Kupfer',
         siede_temperatur: 2562,
         dichte: 5,
