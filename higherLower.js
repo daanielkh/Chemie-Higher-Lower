0e1e3a6645982a8d5b2a6996526e4c303384284d
Dani K <95299224+DanielKholodov@users.noreply.github.com>
2022-02-23 19:50:08 +0100

    Update higherLower.js

--- higherLower.js
+++ higherLower.js
@@ -1,363 +1,287 @@
 data = [
-  
     {
         name: 'Quecksilber',
-        siede_temperatur: 1500,
-        dichte: 13.54,
-        schmelztemperatur: -38.4
+        follower_count: 1500,
+        description: 13.54,
+        country: -38.4
     },
-  
     {
         name: 'Wasserstoff',
-        siede_temperatur: -253,
-        dichte: 0.09,
-       schmelz_temperatur: -259
-    },   
-  
+        follower_count: -253,
+        description: 0.09,
+        country: -259
+    },
     {
         name: 'Natrium',
-        siede_temperatur: 883,
-        dichte: 0.97,
-        schmelz_temperatur: 97.8
-    },   
-  
+        follower_count: 883,
+        description: 0.97,
+        country: 97.8
+    },
     {
         name: 'Rubidium',
-        siede_temperatur: 688,
-        dichte: 1.53,
-        schmelz_temperatur: 39.3
-    },   
-
-    {
+        follower_count: 688,
+        description: 1.53,
+        country: 39.3
+    },
+      {
         name: 'Caesium',
-        siede_temperatur: 678,
-        dichte: 1.9,
-        schmelz_temperatur: 28.4
-    },   
-     
+        follower_count: 678,
+        description: 1.9,
+        country: 28.4
+    },
     {
         name: 'Beryllium',
-        siede_temperatur: 2970,
-        dichte: 1.85,
-        schmelz_temperatur: 1287
-    },   
+        follower_count: 2970,
+        description: 1.85,
+        country: 1287
+    },
   
     {
         name: 'Magnesium',
-        siede_temperatur: 1090,
-        dichte: 1.75,
-        schmelz_temperatur: 650
-    },   
-  
+        follower_count: 1090,
+        description: 1.75,
+        country: 650
+    },
     {
         name: 'Calcium',
-        siede_temperatur: 1484,
-        dichte: 1.55,
-        schmelz_temperatur: 842
-    }, 
-  
+        follower_count: 1484,
+        description: 1.55,
+        country: 842
+    },
     {
         name: 'Strontium',
-        siede_temperatur: 1384,
-        dichte: 2.63,
-        schmelz_temperatur: 777
-    },   
-  
+        follower_count: 1384,
+        description: 2.63,
+        country: 777
+    },
     {
         name: 'Barium',
-        siede_temperatur: 1140,
-        dichte: 3.62,
-        schmelz_temperatur: 727
-    },  
-  
-    {
+        follower_count: 1140,
+        description: 3.62,
+        country: 727
+    }, {
         name: 'Bor',
-        siede_temperatur: 2550,
-        dichte: 2.46,
-        schmelz_temperatur: 2076
-    },  
-    
-    {
+        follower_count: 2550,
+        description: 2.46,
+        country: 2076
+    },
+ {
         name: 'Gallium',
-        siede_temperatur: 2403,
-        dichte: 5.9,
-        schmelz_temperatur: 29.8
-    }, 
-  
-    {
+        follower_count: 2403,
+        description: 5.9,
+        country: 29.8
+    },
+        {
         name: 'Indium',
-        siede_temperatur: 2000,
-        dichte: 7.31,
-        schmelz_temperatur: 256.6
-    },   
-        
+        follower_count: 2000,
+        description: 7.31,
+        country: 256.6
+    },
+   
     {
         name: 'Thalium',
-        siede_temperatur: 1457,
-        dichte: 11.85,
-        schmelz_temperatur: 304
-    },   
-     
-    {
-        name: 'Kohlenstoff',
-        siede_temperatur: 4827,
-        dichte: x,
-        schmelz_temperatur: 3550
-    },   
-  
+        follower_count: 1457,
+        description: 11.85,
+        country: 304
+    },
     {
         name: 'Silicium',
-        siede_temperatur: 2355,
-        dichte: 2.35,
-        schmelz_temperatur: 1410
-    },   
-  
+        follower_count: 2355,
+        description: 2.35,
+        country: 1410
+    },
     {
         name: 'Sauerstoff',
-        siede_temperatur: -183,
-        dichte: 1.43,
-        schmelz_temperatur: -218
-    },   
-  
+        follower_count: -183,
+        description: 1.43,
+        country: -218
+    },
     {
         name: 'Schwefel',
-        siede_temperatur: 445,
-        dichte: 2.07,
-        schmelz_temperatur: 119
-    }, 
-  
+        follower_count: 445,
+        description: 2.07,
+        country: 119
+    },
     {
         name: 'Selen',
-        siede_temperatur: 685,
-        dichte: 3.61,
-        schmelz_temperatur: 220
-    },  
-  
+        follower_count: 685,
+        description: 3.61,
+        country: 220
+    },
     {
         name: 'Antimon',
-        siede_temperatur: 1750,
-        dichte: 6.7,
-        schmelz_temperatur: 630.6
-    },  
-  
+        follower_count: 1750,
+        description: 6.7,
+        country: 630.6
+    },
     {
         name: 'Stickstoff',
-        siede_temperatur: -196,
-        dichte: 1.25,
-        schmelz_temperatur: -210
-    },   
-  
+        follower_count: -196,
+        description: 1.25,
+        country: -210
+    },
     {
         name: 'Fluor',
-        siede_temperatur: -188,
-        dichte: 1.72,
-        schmelz_temperatur: -220
-    },   
-  
+        follower_count: -188,
+        description: 1.72,
+        country: -220
+    },
     {
         name: 'Chlor',
-        siede_temperatur: -35,
-        dichte: 3.22,
-        schmelz_temperatur: 101.5
-    },   
-  
+        follower_count: -35,
+        description: 3.22,
+        country: 101.5
+    },
     {
         name: 'Brom',
-        siede_temperatur: 59,
-        dichte: 3.12,
-        schmelz_temperatur: -7
-    },   
-  
-  
-  
+        follower_count: 59,
+        description: 3.12,
+        country: -7
+    },
     {
         name: 'Wasser',
-        siede_temperatur: 100,
-        dichte: 1,
-        schmelz_temperatur: 0
-        
+        follower_count: 100,
+        description: 1,
+        country: 0
     },
-    
-    
     {
         name: 'Ethanol',
-        siede_temperatur: 78.4,
-        dichte: 0.79,
-        schmelz_temperatur: -114.1
-        
+        follower_count: 78.4,
+        description: 0.79,
+        country: -114.1
     },
-    
-
-    
     {
         name: 'Helium',
-        siede_temperatur: -268.9,
-        dichte: 0.18,
-        schmelz_temperatur: -272.2
-        
+        follower_count: -268.9,
+        description: 0.18,
+        country: -272.2
     },
-    
     {
         name: 'Neon',
-        siede_temperatur: -246.1,
-        dichte: -248,
-        schmelz_temperatur: 0.9
-        
+        follower_count: -246.1,
+        description: -248,
+        country: 0.9
     },
-    
     {
-      name: 'Argon',
-        siede_temperatur: -185.5,
-        dichte: 1.78,
-        schmelz_temperatur: -189.4
-        
+        name: 'Argon',
+        follower_count: -185.5,
+        description: 1.78,
+        country: -189.4
     },
-    
     {
-      name: 'Krypton',
-        siede_temperatur: -153.4,
-        dichte: 3.78,
-        schmelz_temperatur: -157.4
-        
+        name: 'Krypton',
+        follower_count: -153.4,
+        description: 3.78,
+        country: -157.4
     },
-    
     {
-      name: 'Xenon',
-        siede_temperatur: -108.1,
-        dichte: 5.89,
-        schmelz_temperatur: -111.8
-        
+        name: 'Xenon',
+        follower_count: -108.1,
+        description: 5.89,
+        country: -111.8
     },
-    
     {
-      name: 'Phosphor',
-        siede_temperatur: 280.5,
-        dichte: 1.88,
-        schmelz_temperatur: 44.1
-        
+        name: 'Phosphor',
+        follower_count: 280.5,
+        description: 1.88,
+        country: 44.1
     },
-    
     {
-      name: 'Arsen',
-        siede_temperatur: 816.8,
-        dichte: 3.74,
-        schmelz_temperatur: 613.1
-        
+        name: 'Arsen',
+        follower_count: 816.8,
+        description: 3.74,
+        country: 613.1
     },
-    
     {
-      name: 'Germanium',
-        siede_temperatur: 2833,
-        dichte: 5.3,
-        schmelz_temperatur: 938.2
-        
+        name: 'Germanium',
+        follower_count: 2833,
+        description: 5.3,
+        country: 938.2
     },
-    
     {
-      name: 'Iod',
-        siede_temperatur: 184.3,
-        dichte: 4.94,
-        schmelz_temperatur: 113.7
-        
+        name: 'Iod',
+        follower_count: 184.3,
+        description: 4.94,
+        country: 113.7
     },
-    
     {
-      name: 'Zinn',
-        siede_temperatur: 2602,
-        dichte: 7.3,
-        schmelz_temperatur: 231
-        
+        name: 'Zinn',
+        follower_count: 2602,
+        description: 7.3,
+        country: 231
     },
-    
     {
-      name: 'Lithium',
-        siede_temperatur: 1347,
-        dichte: 0.53,
-        schmelz_temperatur: 180
-        
+        name: 'Lithium',
+        follower_count: 1347,
+        description: 0.53,
+        country: 180
     },
-    
     {
-      name: 'Kalium',
-        siede_temperatur: 774,
-        dichte: 0.86,
-        schmelz_temperatur: 63.5
-        
+        name: 'Kalium',
+        follower_count: 774,
+        description: 0.86,
+        country: 63.5
     },
-    
     {
-      name: 'Propan',
-        siede_temperatur: 97,
-        dichte: 0.78,
-        schmelz_temperatur: -126
-        
+        name: 'Propan',
+        follower_count: 97,
+        description: 0.78,
+        country: -126
     },
-    
     {
-      name: 'Methan',
-        siede_temperatur: 65,
-        dichte: 0.72,
-        schmelz_temperatur: -182
-        
+        name: 'Methan',
+        follower_count: 65,
+        description: 0.72,
+        country: -182
     },
-    
     {
-      name: 'Blei',
-        siede_temperatur: 1740,
-        dichte: 11.34,
-        schmelz_temperatur: 327.4
-        
+        name: 'Blei',
+        follower_count: 1740,
+        description: 11.34,
+        country: 327.4
     },
-    
     {
-      name: 'Gold',
-        siede_temperatur: 2700,
-        dichte: 19.3,
-        schmelz_temperatur: 1064
-        
+        name: 'Gold',
+        follower_count: 2700,
+        description: 19.3,
+        country: 1064
     },
-    
     {
-      name: 'Silber',
-        siede_temperatur: 2162,
-        dichte: 10.5,
-        schmelz_temperatur: 660.3
-        
+        name: 'Silber',
+        follower_count: 2162,
+        description: 10.5,
+        country: 660.3
     },
-    
     {
         name: 'Kupfer',
-        siede_temperatur: 2562,
-        dichte: 5,
-        schmelz_temperatur: 5
+        follower_count: 2562,
+        description: 5,
+        country: 5
     }
-    
 ]
-
-
 let aname=document.getElementById("aname")
-let asiede=document.getElementById("asiede")
-let adichte=document.getElementById("adichte")
-let aschmelz = document.getElementById("aschmelz")
+let afollow=document.getElementById("afollow")
+let adescr=document.getElementById("adescr")
+let acount = document.getElementById("acount")
 
 let bname=document.getElementById("bname")
-let bsiede=document.getElementById("bsiede")
-let bdichte=document.getElementById("bdichte")
-let bschmelz = document.getElementById("bschmelz")
+let bfollow=document.getElementById("bfollow")
+let bdescr=document.getElementById("bdescr")
+let bcount = document.getElementById("bcount")
 
 a = random()
 b = random()
 if (a == b && b !== 50) a = b + 1
 else a=b-1
 aname.innerHTML=`<b>Name: </b>${data[a].name}`
-asiede.innerHTML=`<b>Siedetemperatur: </b>${data[a].siede_temperatur} millions`
-adichte.innerHTML=`<b>Dichte: </b>${data[a].dichte}`
-aschmelz.innerHTML=`<b>Schmelztemperatur: </b>${data[a].schmelz_temperatur}`
+afollow.innerHTML=`<b>Siedetemperatur: </b>${data[a].follower_count} °C`
+adescr.innerHTML=`<b>Dichte: </b>${data[a].description} kg/m3`
+acount.innerHTML=`<b>Schmelztemperatur: </b>${data[a].country} °C`
 
 bname.innerHTML=`<b>Name: </b>${data[b].name}`
-bsiede.innerHTML=`<b>Siedetemperatur: </b>? millions`
-bdichte.innerHTML=`<b>Dichte: </b>${data[b].dichte}`
-bschmelz.innerHTML=`<b>Schmelztemperatur: </b>${data[b].schmelz_temperatur}`
+bfollow.innerHTML=`<b>Siedetemperatur: </b>? °C`
+bdescr.innerHTML=`<b>Dichte: </b>${data[b].description} kg/m3`
+bcount.innerHTML=`<b>Schmelztemperatur: </b>${data[b].country} °C`
 
 let hBtn = document.getElementById("high")
 let lBtn = document.getElementById("low")
@@ -365,32 +289,32 @@ let h2=document.querySelector("h2")
 score=0
 hBtn.addEventListener("click", function () {
 
-    if (data[b].siede_temperatur >= data[a].siede_temperatur) {
+    if (data[b].follower_count >= data[a].follower_count) {
         // alert("Yes")
         score++
 
-        bsiede.innerHTML = `<b>Siedetemperatur: </b>${data[b].siede_temperatur} grad CELSIUS`
+        bfollow.innerHTML = `<b>Followers: </b>${data[b].follower_count} °C`
 
-        bsiede.innerHTML = `<b>Siedetemperatur: </b>${data[b].siede_temperatur} grad CELSIUS`
+        bfollow.innerHTML = `<b>Followers: </b>${data[b].follower_count} °C`
         c = random()
         aname.innerHTML=`<b>Name: </b>${data[b].name}`
-        asiede.innerHTML=`<b>Siedetemperatur: </b>${data[b].siede_temperatur} grad CELSIUS`
-        adichte.innerHTML=`<b>Dichte: </b>${data[b].dichte}`
-        aschmelz.innerHTML=`<b>Schmelztemperatur: </b>${data[b].schmelz_temperatur}`
+        afollow.innerHTML=`<b>Followers: </b>${data[b].follower_count} °C`
+        adescr.innerHTML=`<b>Description: </b>${data[b].description} kg/m3`
+        acount.innerHTML=`<b>Country: </b>${data[b].country} °C`
         
         bname.innerHTML=`<b>Name: </b>${data[c].name}`
-        bsiede.innerHTML=`<b>Siedetemperatur: </b>? grad CELSIUS`
-        bdichte.innerHTML=`<b>Dichte: </b>${data[c].dichte}`
-        bschmelz.innerHTML = `<b>Schmelztemperatur: </b>${data[c].schmelz_temperatur}`
+        bfollow.innerHTML=`<b>Followers: </b>? °C`
+        bdescr.innerHTML=`<b>Description: </b>${data[c].description} kg/m3`
+        bcount.innerHTML = `<b>Country: </b>${data[c].country} °C`
         
-        console.log(data[b].siede_temperatur)
-        console.log(data[a].siede_temperatur)
-        data[a].siede_temperatur=data[b].siede_temperatur
+        console.log(data[b].follower_count)
+        console.log(data[a].follower_count)
+        data[a].follower_count=data[b].follower_count
 
         data[b].name=data[c].name
-        data[b].siede_temperatur=data[c].siede_temperatur
-        data[b].dichte=data[c].dichte
-        data[b].schmelz_temperatur = data[c].schmelz_temperatur
+        data[b].follower_count=data[c].follower_count
+        data[b].description=data[c].description
+        data[b].country = data[c].country
         
         
 h2.innerHTML=`Your Total Score is <span>${score}</span>.`
@@ -404,34 +328,34 @@ h2.innerHTML=`Your Total Score is <span>${score}</span>.`
 })
 
 lBtn.addEventListener("click", function () {
-    if (data[b].siede_temperatur <= data[a].siede_temperatur) {
+    if (data[b].follower_count <= data[a].follower_count) {
         // alert("Yes")
         score++
         c = random()
        
         aname.innerHTML=`<b>Name: </b>${data[b].name}`
-        asiede.innerHTML=`<b>Siedetemperatur: </b>${data[b].siede_temperatur} grad CELSIUS`
-        adichte.innerHTML=`<b>Dichte: </b>${data[b].dichte}`
-        aschmelz.innerHTML=`<b>Schmelztemperatur: </b>${data[b].schmelz_temperatur}`
+        afollow.innerHTML=`<b>Followers: </b>${data[b].follower_count} millions`
+        adescr.innerHTML=`<b>Description: </b>${data[b].description}`
+        acount.innerHTML=`<b>Country: </b>${data[b].country}`
         
         bname.innerHTML=`<b>Name: </b>${data[c].name}`
-        bsiede.innerHTML=`<b>Siedetemperatur: </b>? grad CELSIUS`
-        bdichte.innerHTML=`<b>Dichte: </b>${data[c].dichte}`
-        bschmelz.innerHTML = `<b>Schmelztemperatur: </b>${data[c].schmelz_temperatur}`
+        bfollow.innerHTML=`<b>Followers: </b>? millions`
+        bdescr.innerHTML=`<b>Description: </b>${data[c].description}`
+        bcount.innerHTML = `<b>Country: </b>${data[c].country}`
         
-        console.log(data[b].siede_temperatur)
-        console.log(data[a].siede_temperatur)
-        data[a].siede_temperatur=data[b].siede_temperatur
+        console.log(data[b].follower_count)
+        console.log(data[a].follower_count)
+        data[a].follower_count=data[b].follower_count
 
         data[b].name=data[c].name
-        data[b].siede_temperatur=data[c].siede_temperatur
-        data[b].dichte=data[c].dichte
-        data[b].schmelz_temperatur = data[c].schmelz_temperatur
+        data[b].follower_count=data[c].follower_count
+        data[b].description=data[c].description
+        data[b].country = data[c].country
         
         h2.innerHTML=`Your Total Score is <span>${score}</span>.`
     }
     else {
-        alert("Du hast verloren lol")
+        alert("You Loss Game!!!!!!!!!!!")
         score = 0
                 
 h2.innerHTML=`Your Total Score is <span>${score}</span>.`
