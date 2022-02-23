1f1ee221e13895de152de7536522a93195556ae9
Dani K <95299224+DanielKholodov@users.noreply.github.com>
2022-02-23 19:11:51 +0100

    Update higherLower.js

--- higherLower.js
+++ higherLower.js
@@ -4,168 +4,168 @@ data = [
         name: 'Quecksilber',
         siede_temperatur: 1500,
         dichte: 13.54,
-        schmelztemperatur: -38.4,
+        schmelztemperatur: -38.4
     },
   
     {
         name: 'Wasserstoff',
         siede_temperatur: -253,
         dichte: 0.09,
-       schmelz_temperatur: -259,
+       schmelz_temperatur: -259
     },   
   
     {
         name: 'Natrium',
         siede_temperatur: 883,
         dichte: 0.97,
-        schmelz_temperatur: 97.8,
+        schmelz_temperatur: 97.8
     },   
   
     {
         name: 'Rubidium',
         siede_temperatur: 688,
         dichte: 1.53,
-        schmelz_temperatur: 39.3,
+        schmelz_temperatur: 39.3
     },   
 
     {
         name: 'Caesium',
         siede_temperatur: 678,
         dichte: 1.9,
-        schmelz_temperatur: 28.4,
+        schmelz_temperatur: 28.4
     },   
      
     {
         name: 'Beryllium',
         siede_temperatur: 2970,
         dichte: 1.85,
-        schmelz_temperatur: 1287,
+        schmelz_temperatur: 1287
     },   
   
     {
         name: 'Magnesium',
         siede_temperatur: 1090,
         dichte: 1.75,
-        schmelz_temperatur: 650,
+        schmelz_temperatur: 650
     },   
   
     {
         name: 'Calcium',
         siede_temperatur: 1484,
         dichte: 1.55,
-        schmelz_temperatur: 842,
+        schmelz_temperatur: 842
     }, 
   
     {
         name: 'Strontium',
         siede_temperatur: 1384,
         dichte: 2.63,
-        schmelz_temperatur: 777,
+        schmelz_temperatur: 777
     },   
   
     {
         name: 'Barium',
         siede_temperatur: 1140,
         dichte: 3.62,
-        schmelz_temperatur: 727,
+        schmelz_temperatur: 727
     },  
   
     {
         name: 'Bor',
         siede_temperatur: 2550,
         dichte: 2.46,
-        schmelz_temperatur: 2076,
-    },   
+        schmelz_temperatur: 2076
+    },  
     
     {
         name: 'Gallium',
         siede_temperatur: 2403,
         dichte: 5.9,
-        schmelz_temperatur: 29.8,
+        schmelz_temperatur: 29.8
     }, 
   
     {
         name: 'Indium',
         siede_temperatur: 2000,
         dichte: 7.31,
-        schmelz_temperatur: 256.6,
+        schmelz_temperatur: 256.6
     },   
         
     {
         name: 'Thalium',
         siede_temperatur: 1457,
         dichte: 11.85,
-        schmelz_temperatur: 304,
+        schmelz_temperatur: 304
     },   
      
     {
         name: 'Kohlenstoff',
         siede_temperatur: 4827,
         dichte: x,
-        schmelz_temperatur: 3550,
+        schmelz_temperatur: 3550
     },   
   
     {
         name: 'Silicium',
         siede_temperatur: 2355,
         dichte: 2.35,
-        schmelz_temperatur: 1410,
+        schmelz_temperatur: 1410
     },   
   
     {
         name: 'Sauerstoff',
         siede_temperatur: -183,
         dichte: 1.43,
-        schmelz_temperatur: -218,
+        schmelz_temperatur: -218
     },   
   
     {
         name: 'Schwefel',
         siede_temperatur: 445,
         dichte: 2.07,
-        schmelz_temperatur: 119,
+        schmelz_temperatur: 119
     }, 
   
     {
         name: 'Selen',
         siede_temperatur: 685,
         dichte: 3.61,
-        schmelz_temperatur: 220,
+        schmelz_temperatur: 220
     },  
   
     {
         name: 'Antimon',
         siede_temperatur: 1750,
         dichte: 6.7,
-        schmelz_temperatur: 630.6,
+        schmelz_temperatur: 630.6
     },  
   
     {
         name: 'Stickstoff',
         siede_temperatur: -196,
         dichte: 1.25,
-        schmelz_temperatur: -210,
+        schmelz_temperatur: -210
     },   
   
     {
         name: 'Fluor',
         siede_temperatur: -188,
         dichte: 1.72,
-        schmelz_temperatur: -220,
+        schmelz_temperatur: -220
     },   
   
     {
         name: 'Chlor',
         siede_temperatur: -35,
         dichte: 3.22,
-        schmelz_temperatur: 101.5,
+        schmelz_temperatur: 101.5
     },   
   
     {
         name: 'Brom',
         siede_temperatur: 59,
         dichte: 3.12,
-        schmelz_temperatur: -7,
+        schmelz_temperatur: -7
     },   
   
   
